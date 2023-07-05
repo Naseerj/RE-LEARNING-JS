5d48cfd5b5b925ca9402542f332e13f92857e163
Naseerj <jenrolaolasupo@gmail.com>
2023-07-05 13:12:38 +0100

    learning js/datatypes

---
+++ index.js
@@ -0,0 +1,56 @@
+const name = 'olasupoJenrola' /* STRING */
+const num = 67 /* NUMBER */
+
+const text = 'CHATGPT IS \'ALSO\' FOR MANY  \n THINGS \t SHA'
+const length = text.length
+console.log(length)
+console.log(text)
+
+const test = "A newline character is written like \"\\n\" ymll."
+console.log(test)
+/* A newline character is written like
+"\" */
+
+const maths = `half of 100 is ${100 / 2}`
+console.log(maths)
+ 
+console.log('aang' > 'zang')
+console.log("Aardvark" < "Zoroaster")
+
+console.log(undefined == undefined)
+
+console.log(false ? 1 : 2);
+
+let x = 16 + "Volvo";
+console.log(x)
+
+
+/* NULL && UNDEFINED */
+
+let nully = ''
+console.log(typeof(nully))
+
+let undefined1;
+console.log(typeof(undefined1))
+
+
+console.log('1' + 5)
+console.log(1 + '5')
+
+console.log(null == null)
+
+
+
+
+/* DATATYPES --  
+PRIMTIVE 
+-STRING
+-NUMBER
+-BOOLEAN
+-SYMBOL
+-
+
+
+NON PRIMITIVE 
+ OBJECTS 
+ ARRAYS */
